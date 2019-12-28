e2427afe6ae6711d9790c29152ff745a68b1ab4d
todd-ddd <todd.ddd@kakaocorp.com>
2019-12-28 15:31:17 +0900

    Add arrow core

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,10 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
 	kotlin("jvm") version "1.3.61"
 	kotlin("plugin.spring") version "1.3.61"
+	kotlin("kapt") version "1.3.61"
 }
+
+apply(plugin = "kotlin-kapt")
 
 group = "com.eunmin"
 version = "0.0.1-SNAPSHOT"
@@ -20,7 +23,10 @@ configurations {
 
 repositories {
 	mavenCentral()
+	maven(url = "https://dl.bintray.com/arrow-kt/arrow-kt/")
 }
+
+val arrow_version = "0.10.4"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
@@ -30,6 +36,9 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("io.springfox:springfox-swagger2:2.9.2")
 	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+	implementation("io.arrow-kt:arrow-core:$arrow_version")
+	implementation("io.arrow-kt:arrow-syntax:$arrow_version")
+	kapt("io.arrow-kt:arrow-meta:$arrow_version")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
